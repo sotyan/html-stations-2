edc31336814a81975212251d4703dc01e73b1765
sotyan <happy.tanbarin2@gmail.com>
2024-09-09 21:15:19 +0900

    Station13,complete

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,8 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  if(e.target.checked){
+    document.getElementById("text").style.backgroundColor = "red";
+  }else {
+    document.getElementById("text").style.backgroundColor = "white";
+  }
 }
